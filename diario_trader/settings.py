87a1ebc347856e75a2378c8f3c245959f5ee6c74
gutomattos <jguto21@gmail.com>
2025-06-15 23:48:28 -0300

    Ajusta settings.py e adiciona superusuário ao banco SQLite

--- diario_trader/settings.py
+++ diario_trader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ars)1_guzgt1$-&)s#ca-@6c4u=p--mssbkxj^i(x479udfaue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
 
 # Application definition
